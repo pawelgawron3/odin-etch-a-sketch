e9d7a94e6de7e8d80c748d87dc4484fd487f87b5
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-20 12:56:00 +0200

    Refactor the script code

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let btn = document.querySelector("#create-grid");
 const containerWidth = container.clientWidth;
 const containerHeight = container.clientHeight;
 
-function createGrid(size) {
+function createGrid(side) {
   container.replaceChildren();
-  let numOfElements = size ** 2;
-  for (let i = 0; i < numOfElements; i++) {
+  let size = side ** 2;
+  for (let i = 0; i < size; i++) {
     let rndCol1 = Math.floor(Math.random() * 256);
     let rndCol2 = Math.floor(Math.random() * 256);
     let rndCol3 = Math.floor(Math.random() * 256);
 
     let innerDiv = document.createElement("div");
-    innerDiv.style.width = containerWidth / Math.sqrt(numOfElements) + "px";
-    innerDiv.style.height = containerHeight / Math.sqrt(numOfElements) + "px";
+    innerDiv.style.width = containerWidth / Math.sqrt(size) + "px";
+    innerDiv.style.height = containerHeight / Math.sqrt(size) + "px";
     innerDiv.addEventListener("mouseenter", () => {
       innerDiv.style.backgroundColor = `rgb(
         ${rndCol1},
@@ -31,18 +31,14 @@ btn.addEventListener("click", () => {
     "Select a number of elements per side for new grid: ",
     0
   );
-  if (userInput.includes(".") || userInput.includes(",")) {
-    alert("Error! Please enter a positive, integer number!");
+  let side = parseInt(userInput);
+  if (isNaN(side) || side < 0) {
+    alert("Error! Please enter a positive number!");
+  } else if (side >= 100) {
+    alert(
+      "Due to potential performance issues please enter a number lower than 100."
+    );
   } else {
-    let size = parseInt(userInput);
-    if (size >= 100) {
-      alert(
-        "Due to potential performance issues please enter a number lower than 100."
-      );
-    } else if (isNaN(size) || size < 0) {
-      alert("Error! Please enter a positive, integer number!");
-    } else {
-      createGrid(size);
-    }
+    createGrid(side);
   }
 });
